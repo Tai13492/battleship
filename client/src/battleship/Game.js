c35d13a7a70a0f914bdefddf6dea250e597cff29
Tai13492 <friendship045@gmail.com>
2018-11-03 21:25:54 +0700

    bug fix explosion not find

--- client/src/battleship/Game.js
+++ client/src/battleship/Game.js
@@ -15,27 +15,29 @@ class Game extends React.Component {
 				const isEmpty = squares[x][y].ship === null;
 				const { isHit } = squares[x][y];
 				return (
-					<div
-						key={'' + x + y}
-						style={{ width: '12.5%', height: '12.5%' }}
-					>
-						<BoardSquare
-							onClick={() => console.log(`x:${x},y:${y}`)}
-							isEmpty={isEmpty}
-							isWhite={isHit && isEmpty}
+					console.log(isHit && !isEmpty) || (
+						<div
+							key={'' + x + y}
+							style={{ width: '12.5%', height: '12.5%' }}
 						>
-							{!isEmpty && (
-								<img
-									src={squares[x][y].ship.shipPart}
-									alt="ship_part"
-								/>
-							)}
-							{!isEmpty &&
-								isHit && (
+							<BoardSquare
+								onClick={() => console.log(`x:${x},y:${y}`)}
+								isEmpty={isEmpty}
+								isWhite={isHit && isEmpty}
+							>
+								{isHit && !isEmpty ? (
 									<img src={explosion} alt="explosion" />
+								) : (
+									!isEmpty && (
+										<img
+											src={squares[x][y].ship.shipPart}
+											alt="ship_part"
+										/>
+									)
 								)}
-						</BoardSquare>
-					</div>
+							</BoardSquare>
+						</div>
+					)
 				);
 			})
 		);
@@ -74,10 +76,9 @@ class Game extends React.Component {
 							}}
 							isWhite={isHit && isEmpty}
 						>
-							{isHit &&
-								!isEmpty && (
-									<img src={explosion} alt="explosion" />
-								)}
+							{isHit && !isEmpty ? (
+								<img src={explosion} alt="explosion" />
+							) : null}
 						</OpponentBoardSquare>
 					</div>
 				);
